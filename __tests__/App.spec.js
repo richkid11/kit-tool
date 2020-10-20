d1983e0d9bc7a8dc09bed65cd8c0f5c0ede669a5
son2412 <ngovanson196@gmail.com>
2020-10-20 11:49:00 +0700

    test deploy

---
+++ __tests__/App.spec.js
@@ -0,0 +1,5 @@
+describe('App', () => {
+  it('First Test', () => {
+    console.log('https://www.profit4cloud.nl/blog/automatically-publish-npm-packages-with-travis-ci/');
+  });
+});
--- app/Console/Commands/GitHub/GitBase.js
+++ app/Console/Commands/GitHub/GitBase.js
@@ -1,9 +1,11 @@
 import ConfigurationRepository from '../../../Repositories/ConfigurationRepository';
 import octikit from '@octokit/rest';
+import { Error } from '../Command';
 
 export class GitBase {
   async authGit() {
     const git_token_db = await new ConfigurationRepository().getValue('git_token');
+    if (!git_token_db || !git_token_db.value) Error('Setup config git, please !');
     const clientWithAuth = new octikit({
       auth: git_token_db.value
     });
